5829b3594e83ec9f3e6703d9466f01a843f29380
tuss4 <tuss4dbfn@gmail.com>
2014-07-07 20:07:30 +0000

    playing with fabric

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,31 @@
-from fabric.api import local
+from fabric.api import local, env, cd, run, sudo
+
+
+env.hosts = ['ec2-54-191-20-55.us-west-2.compute.amazonaws.com']
+env.user = 'ubuntu'
+env.key_filename = '/vagrant/codelabtj.cer'
+
+def test():
+    local("./manage.py test")
+
+
+def commit():
+    local("git add -p && git commit")
+
+
+def push():
+    local("git push origin master")
+
+
+def deploy():
+    code_dir = '/home/ubuntu/drf_lab'
+    with cd(code_dir):
+        sudo("service fig-example stop")
+        run("git pull origin master")
+        sudo("service fig-example start")
+
 
 def prepare_deploy():
-    local("./manage.py test")
+    test()
+    commit()
+    push()
